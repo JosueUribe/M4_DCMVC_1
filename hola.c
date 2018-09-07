fc9401fb2e1c59467704841266afd69c18a6e305
Victor Alvarado <victor.alvarado.dse@uteq.edu.mx>
2018-09-07 14:32:10 -0500

    Add enumeration and revision Log

--- hola.c
+++ hola.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 
 int cd_player(int g, int c);
+
 int main()
 {
+
+Typedef enum
+{
+	Monday,
+	Tuesday,
+}WeekDays_T;
 
 printf("\n hola nuevamente \n");
 printf("adios");
 return 0;
 
 }
+
+/********************************************Revision LOG****************************************************/
+/* 07 - Sept - 2018 
+/* Victor Alvarado - Add Revision Log to file
+/*
+/*
+************************************************************************************************************/
